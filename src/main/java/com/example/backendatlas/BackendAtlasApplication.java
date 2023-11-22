179f303e0b6fe33606eb98f129e84c22365d9dd4
ahma4760 <ahma4760@stud.kea.dk>
2023-11-22 13:08:59 +0100

    Initial commit

---
+++ src/main/java/com/example/backendatlas/BackendAtlasApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAtlasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAtlasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendatlas/BackendAtlasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAtlasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
