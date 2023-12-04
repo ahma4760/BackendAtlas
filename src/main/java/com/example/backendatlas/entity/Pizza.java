64f35f12e455df4afa2abd2955893cd78ede391c
Mikael H <mikael.holsve@gmail.com>
2023-12-04 14:25:04 +0100

    Create pizza entity class

---
+++ src/main/java/com/example/backendatlas/entity/Pizza.java
@@ -0,0 +1,68 @@
+package com.example.backendatlas.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String ingredients;
+    private String imagePath;
+    private double price;
+
+    public Pizza() {
+    }
+
+    public Pizza(int id, String title, String ingredients, String imagePath, double price) {
+        this.id = id;
+        this.title = title;
+        this.ingredients = ingredients;
+        this.imagePath = imagePath;
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
