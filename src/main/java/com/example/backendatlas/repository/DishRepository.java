75e505b29d8cf7991ba588fe1cec7b7efc2db463
Mikael H <mikael.holsve@gmail.com>
2023-12-04 14:30:05 +0100

    Create DishRepository.java

---
+++ src/main/java/com/example/backendatlas/repository/DishRepository.java
@@ -0,0 +1,10 @@
+package com.example.backendatlas.repository;
+
+import com.example.backendatlas.entity.Dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DishRepository extends JpaRepository<Dish, Long> {
+    List<Dish> findAll();
+}
