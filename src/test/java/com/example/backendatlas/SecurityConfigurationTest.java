016ee670f4e41fa98f265193accbf652d1b3e70f
obijohnkanobi <119489622+obijohnkanobi@users.noreply.github.com>
2023-12-11 10:16:58 +0100

    Lavet test af Jwt Security

---
+++ src/test/java/com/example/backendatlas/SecurityConfigurationTest.java
@@ -0,0 +1,43 @@
+package com.example.backendatlas;
+
+import com.example.backendatlas.config.SecurityConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class SecurityConfigurationTest {
+
+    @Autowired
+    private SecurityConfiguration securityConfiguration;
+
+    @Test
+    public void passwordEncoderBean_exists() {
+        assertNotNull(SecurityConfiguration.passwordEncoder());
+    }
+
+    @Test
+    public void filterChainBean_correctlyConfigured() {
+        assertDoesNotThrow(() -> securityConfiguration.filterChain(null));
+    }
+
+    @Test
+    public void authenticationManagerBean_exists() {
+        assertDoesNotThrow(() -> securityConfiguration.authenticationManager(null));
+    }
+
+    @Test
+    public void corsConfiguration_isCorrect() {
+        CorsRegistry registry = new CorsRegistry();
+        securityConfiguration.addCorsMappings(registry);
+        // Her skal du tilføje yderligere checks for at bekræfte, at CORS konfigurationen er korrekt.
+    }
+}
+
---
+++ src/test/java/com/example/backendatlas/UserServiceTest.java
@@ -0,0 +1,100 @@
+package com.example.backendatlas;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import com.example.backendatlas.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import com.example.backendatlas.entity.User;
+import com.example.backendatlas.repository.UserRepository;
+import java.util.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void testFindAll() {
+        Set<User> expectedUsers = new HashSet<>();
+        expectedUsers.add(new User()); // Tilføj nogle mock-brugere
+        when(userRepository.findAll()).thenReturn((List<User>) expectedUsers);
+
+        Set<User> actualUsers = userService.findAll();
+
+        assertEquals(expectedUsers, actualUsers);
+        verify(userRepository).findAll();
+    }
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setPassword("rawPassword");
+
+        when(passwordEncoder.encode(anyString())).thenReturn("encodedPassword");
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        User savedUser = userService.save(user);
+
+        assertEquals("encodedPassword", savedUser.getPassword());
+        verify(passwordEncoder).encode("rawPassword");
+        verify(userRepository).save(user);
+    }
+    @Test
+    public void testDelete() {
+        User user = new User();
+
+        userService.delete(user);
+
+        verify(userRepository).delete(user);
+    }
+    @Test
+    public void testDeleteById() {
+        Long userId = 1L;
+
+        userService.deleteById(userId);
+
+        verify(userRepository).deleteById(userId);
+    }
+    @Test
+    public void testFindById() {
+        Long userId = 1L;
+        Optional<User> expectedUser = Optional.of(new User());
+
+        when(userRepository.findById(userId)).thenReturn(expectedUser);
+
+        Optional<User> actualUser = userService.findById(userId);
+
+        assertEquals(expectedUser, actualUser);
+        verify(userRepository).findById(userId);
+    }
+    @Test
+    public void testFindByName() {
+        String username = "testUser";
+        List<User> expectedUsers = Arrays.asList(new User());
+
+        when(userRepository.findByUsername(username)).thenReturn(expectedUsers);
+
+        List<User> actualUsers = userService.findByName(username);
+
+        assertEquals(expectedUsers, actualUsers);
+        verify(userRepository).findByUsername(username);
+    }
+
+
+
+
+}
+
